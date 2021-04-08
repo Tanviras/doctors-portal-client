dc84e19c8f9a35173210c1c2191e6246db829e5a
Tanviras <tanvirasme@gmail.com>
2021-04-08 17:57:24 +0600

    all functionalities done

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtGHP2qUfihfYK3_3R2t3hIONQAnIqHvk",
+  authDomain: "doctors-portal-28ac3.firebaseapp.com",
+  projectId: "doctors-portal-28ac3",
+  storageBucket: "doctors-portal-28ac3.appspot.com",
+  messagingSenderId: "444930491131",
+  appId: "1:444930491131:web:ce146873103290e0bae65e"
+};
+
+export default firebaseConfig;
