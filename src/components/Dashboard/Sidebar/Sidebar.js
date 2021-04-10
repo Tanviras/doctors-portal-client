7e39ce420b51ba03a0aaa4f27d1d4d0bb3b1dcc1
Tanviras <tanvirasme@gmail.com>
2021-04-10 16:24:20 +0600

    sign out, appointments show

--- src/components/Dashboard/Sidebar/Sidebar.js
+++ src/components/Dashboard/Sidebar/Sidebar.js
@@ -66,7 +66,7 @@ const Sidebar = () => {
                         </Link>
                     </li>
                 </div>
-                }
+                 }
                 
             </ul>
             <div>
